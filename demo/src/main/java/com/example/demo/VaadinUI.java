42bcdc025b64f646bd8b7e663771f18ab30d0de0
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2019-05-24 15:33:35 +0300

    add ui works

--- demo/src/main/java/com/example/demo/VaadinUI.java
+++ demo/src/main/java/com/example/demo/VaadinUI.java
@@ -9,6 +9,7 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -26,7 +27,11 @@ public class VaadinUI extends UI {
     private TextField firstName = new TextField("First name");
     private TextField lastName = new TextField("Last name");
     private Button save = new Button("Save", e -> saveCustomer());
-
+//    omaa shaissee
+    private Button add =  new Button("add", e -> setAddFormVisible(true));
+    private TextField firstNameAdd = new TextField("First name");
+    private TextField lastNameAdd = new TextField("Last name");
+    private Button saveNewCustomer = new Button("save");
     @Override
     protected void init(VaadinRequest request) {
         updateGrid();
@@ -35,30 +40,38 @@ public class VaadinUI extends UI {
 
         binder.bindInstanceFields(this);
 
-        VerticalLayout layout = new VerticalLayout(grid, firstName, lastName, save);
-        setContent(layout);
+        VerticalLayout layout = new VerticalLayout(add, grid, firstName, lastName, save);
+        VerticalLayout addForm = new VerticalLayout(firstNameAdd, lastNameAdd, saveNewCustomer);
+        HorizontalLayout main = new HorizontalLayout(layout, addForm);
+        setContent(main);
     }
 
     private void updateGrid() {
         List<Customer> customers = service.findAll();
         grid.setItems(customers);
-        setFormVisible(false);
+        setEditFormVisible(false);
+        setAddFormVisible(false);
     }
 
     private void updateForm() {
         if (grid.asSingleSelect().isEmpty()) {
-            setFormVisible(false);
+            setEditFormVisible(false);
         } else {
             customer = grid.asSingleSelect().getValue();
             binder.setBean(customer);
-            setFormVisible(true);
+            setEditFormVisible(true);
         }
     }
 
-    private void setFormVisible(boolean visible) {
+    private void setEditFormVisible(boolean visible) {
         firstName.setVisible(visible);
         lastName.setVisible(visible);
         save.setVisible(visible);
+    }
+    private void setAddFormVisible(boolean visible) {
+        firstNameAdd.setVisible(visible);
+        lastNameAdd.setVisible(visible);
+        saveNewCustomer.setVisible(visible);
     }
 
     private void saveCustomer() {
